562fdedcc580d56b8f33c4e8f1ec7557da47c353
hxmbl <180758703+Hxmbl@users.noreply.github.com>
2026-02-10 23:48:45 +0000

    a

--- src/modules/passbrute/core.py
+++ src/modules/passbrute/core.py
@@ -81,6 +81,8 @@ def passbrute():
 
     with open(wordlist_path, 'r', encoding="utf-8") as f:
         passwlist = [line.strip() for line in f]
+        # Make this remove empty lines and spaces, also make |>> text<<| a comment in the wordlist file so that you can add comments to the wordlist file and it will ignore them.
+        passwlist = [line.strip() for line in passwlist if line.strip() and not (line.startswith("|>> ") and line.endswith(" <<|"))]
 
     if not wordlist_path:
         typer.echo("Using default brute force ")
